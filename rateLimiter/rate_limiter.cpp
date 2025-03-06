4a2f8c56f99e9a181a1867859432f18c47580d69
079035 <sroot079@gmail.com>
2025-03-06 20:46:19 +0000

    vatic

---
+++ rateLimiter/rate_limiter.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class RateLimiter {
+public:
+    RateLimiter(size_t N, uint64_t T): num_events_(N), time_window_(T) {}
+
+    bool check(uint64_t now) {
+        timestamps_.pop();
+        timestamps_.push(now);
+
+        if (time_window_ > now - timestamps_.front()) {
+            return false;
+        }
+        return true;
+    }
+
+private:
+    uint64_t time_window_;
+    size_t num_events_;
+    queue<uint64_t> timestamps_;
+};
+
+int main() {
+    
+}
